cb78b978de1fd9dbd7c4d352f91162363c792438
umbra-jaco <jacobblxndr@gmail.com>
2026-04-22 22:51:24 -0700

    Phase 02 completed. Acoustic Node added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* Keep this empty or use it for standard Next.js features only */
 };
 
 export default nextConfig;
